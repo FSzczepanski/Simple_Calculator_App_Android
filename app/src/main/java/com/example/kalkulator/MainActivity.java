d9fe8c725a0371a9c72e42aeffa78d97142307bf
FSzczepanski <filip.szczepanskipv@gmail.com>
2020-04-29 13:40:37 +0200

    add bug fix

--- app/src/main/java/com/example/kalkulator/MainActivity.java
+++ app/src/main/java/com/example/kalkulator/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
+//master
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
